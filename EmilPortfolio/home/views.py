61601f789b6e9b157ba170ed13eff8804739f738
EmilSantos <Santos.N.Emil@gmail.com>
2021-12-04 15:54:07 -0800

    Contact page Model and Admind
    created Contact table and made migrations. Contact Page has been setup to stave too db. Admin and superuser, created.

--- EmilPortfolio/home/views.py
+++ EmilPortfolio/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
 
 def home(request):
   #return HttpResponse("This is my homepage (/)")
@@ -14,5 +15,16 @@ def projects(request):
   return render(request, 'projects.html')
 
 def contact(request):
+  if request.method=="POST":
+    print("This is post")
+    name = request.POST["name"]
+    email = request.POST["email"]
+    phone = request.POST["phone"]
+    desc = request.POST["desc"]
+    print(name, email, phone, desc)
+    contact = Contact(name=name, email=email, phone=phone, desc=desc)
+    contact.save()
+    print("the data has been written to the db")
+
   #return HttpResponse("This is my contact (/contact)")
   return render(request, 'contact.html')
