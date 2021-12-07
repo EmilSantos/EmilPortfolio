3a156f93dc252e2c4b7c1e404f47688d1f95a48c
EmilSantos <Santos.N.Emil@gmail.com>
2021-12-07 13:06:16 -0800

    Admin, navbar changes
    Changed some admin page style and add github link

--- EmilPortfolio/home/urls.py
+++ EmilPortfolio/home/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from home import views
 
+# Django admin header cutomization
+admin.site.site_header = "Emil's Dashboard"
+admin.site.site_title = "Emil's Dashboard"
+admin.site.index_title = "Welcome to Portal"
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
